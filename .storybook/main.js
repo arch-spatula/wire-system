5f5982391c90043b3b6983896ca78c91721ae140
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-05 22:01:06 +0900

    chore: telemetry 방지 #12

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,24 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/vue3-vite",
+    name: '@storybook/vue3-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+    disableTelemetry: true, // 👈 Disables telemetry
   },
 };
 export default config;
