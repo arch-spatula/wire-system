d50ae958833f2e198e545643a109d2cf0eda1d54
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-09 10:05:21 +0900

    feat: input 양방향 바인딩 지원 및 스타일링 #13

--- stories/Input.stories.js
+++ stories/Input.stories.js
@@ -2,25 +2,10 @@ import MyInput from './Input.vue';
 // import type { Meta } from '@storybook/vue3';
 
 const meta = {
-  // title: 'Example/Input',
   component: MyInput,
-  // tags: ['autodocs'],
-  // argTypes: {
-  //   backgroundColor: {
-  //     control: 'color',
-  //   },
-  //   onClick: {},
-  //   size: {
-  //     control: {
-  //       type: 'select',
-  //     },
-  //     options: ['small', 'medium', 'large'],
-  //   },
-  // },
 };
 export default meta;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   // components: { MyInput },
   args: {
