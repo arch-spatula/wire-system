c414aeea8855a83daf5e13e9950358b535ac7dbc
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-09 10:05:21 +0900

    feat: input 양방향 바인딩 지원 및 스타일링 #13

--- stories/Input.vue
+++ stories/Input.vue
@@ -1,13 +1,24 @@
 <template>
-  <input v-model="text" />
+  <input class="input" type="text" v-model="text" />
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-
-const text = ref('');
-
-watch(text, () => {
-  console.log(text.value);
-});
+const text = defineModel('text');
 </script>
+
+<style scoped lang="scss">
+.input {
+  all: unset;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  padding: 8px 12px;
+  box-sizing: border-box;
+  box-shadow: 0 0 0 1px #6b7280 inset;
+  &:hover {
+    box-shadow: 0 0 0 2px #6b7280 inset;
+  }
+  &:focus {
+    box-shadow: 0 0 0 2px #111827 inset;
+  }
+}
+</style>
