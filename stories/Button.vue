b6e65d254cef7133c0365bbeddc1779a39aeacd8
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-28 22:23:26 +0900

    0.1.0 alpha (#28)
    * chore: telemetry 방지 #12
    
    * chore: 캐시 삭제 및 gitignore에 추가 #12
    
    * feat: input 파일 생성 및 초기화 #13
    
    * feat: input 양방향 바인딩 지원 및 스타일링 #13
    
    * Feat/font (#20)
    
    * doc: 문서 초기화 #16
    
    * feat: 폰트 클래스 추가 #16
    
    * doc: 폰트 설명 추가 #16
    
    * Doc/intro (#21)
    
    * chore: 이미지 제거 및 파일명 수정 #18
    
    * chore: intro 문서 순서 변경 #18
    
    * Feat/text button (#22)
    
    * feat: 텍스트 버튼 구현 #17
    
    * feat: 버튼 사이즈 잠시 가리기 #17
    
    * Doc:아이콘 예시 문서 추가 #24 (#27)

--- stories/Button.vue
+++ stories/Button.vue
@@ -4,84 +4,79 @@
   </button>
 </template>
 
-<script>
+<script setup lang="ts">
 import './button.css';
-import { reactive, computed } from 'vue';
+import { computed, defineProps, withDefaults, toRefs, defineEmits } from 'vue';
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    primary: boolean;
+    // size: 'small' | 'medium' | 'large';
+    backgroundColor: string;
+  }>(),
+  {
+    label: '',
+    primary: false,
+    // size: 'large',
+  }
+);
 
-export default {
-  name: 'my-button',
+const { label, primary, backgroundColor } = toRefs(props);
 
-  props: {
-    label: {
-      type: String,
-      required: true,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
-    },
-    backgroundColor: {
-      type: String,
-    },
-  },
+const emit = defineEmits(['click']);
 
-  emits: ['click'],
+const classes = computed(() => ({
+  button: true,
+  'storybook-button--primary': primary.value,
+  'storybook-button--secondary': !primary.value,
+  // [`storybook-button--${size.value || 'medium'}`]: true,
+  'body-2-r': true,
+}));
 
-  setup(props, { emit }) {
-    props = reactive(props);
-    return {
-      classes: computed(() => ({
-        'storybook-button': true,
-        'storybook-button--primary': props.primary,
-        'storybook-button--secondary': !props.primary,
-        [`storybook-button--${props.size || 'medium'}`]: true,
-      })),
-      style: computed(() => ({
-        backgroundColor: props.backgroundColor,
-      })),
-      onClick() {
-        emit('click');
-      },
-    };
-  },
+const style = computed(() => ({
+  backgroundColor: backgroundColor.value,
+}));
+
+const onClick = () => {
+  emit('click');
 };
 </script>
 
 <style scoped lang="scss">
-.storybook-button {
-  /* font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; */
-  font-weight: 700;
+.button {
   border: 0;
-  border-radius: 3em;
+  border-radius: 10px;
   cursor: pointer;
   display: inline-block;
   line-height: 1;
+  width: 160px;
+  height: 48px;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 28px;
 }
 .storybook-button--primary {
   color: white;
-  background-color: #10b981;
+  background-color: #2d2d2d;
 }
 .storybook-button--secondary {
-  color: #333;
+  color: #2d2d2d;
   background-color: transparent;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  /* box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset; */
+  box-shadow: 0 0 0 1px #2d2d2d inset;
 }
 .storybook-button--small {
-  font-size: 12px;
-  padding: 10px 16px;
+  /* font-size: 12px; */
+  /* padding: 10px 16px; */
 }
 .storybook-button--medium {
-  font-size: 14px;
-  padding: 11px 20px;
+  /* font-size: 14px; */
+  /* padding: 11px 20px; */
 }
 .storybook-button--large {
-  font-size: 16px;
-  padding: 12px 24px;
+  /* font-size: 16px; */
+  /* padding: 12px 24px; */
 }
 </style>
